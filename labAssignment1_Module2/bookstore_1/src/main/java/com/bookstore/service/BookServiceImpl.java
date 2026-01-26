cb15cec393148741d185a0cb6cb2691b7b440b9c
TanejaBhavya <btaneja@yamaha-motor-india.com>
2026-01-26 23:33:13 +0530

    Add files via upload

---
+++ labAssignment1_Module2/bookstore_1/src/main/java/com/bookstore/service/BookServiceImpl.java
@@ -0,0 +1,95 @@
+package com.bookstore.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.bookstore.dao.BookJdbcImpl;
+import com.bookstore.dao.BookDao;
+import com.bookstore.dao.DaoException;
+import com.bookstore.dto.Book;
+
+public class BookServiceImpl implements BookService {
+	
+	private BookDao bookDao;
+	public BookServiceImpl() {
+		try {
+			bookDao = new BookJdbcImpl();
+		} catch (DaoException e) {
+			throw new RuntimeException("BookService can't be initiated",e);
+		}
+	}
+	
+	@Override
+	public List<Book> getAllBooks() {
+		try {
+			return bookDao.getAllBooks();
+		}catch(DaoException e) {
+			throw new RuntimeException("Unable to display books",e);
+		}
+	}
+
+	@Override
+	public Book addBook(Book book) {
+		bookValidation(book);
+		try {
+			return bookDao.addBook(book);
+		}catch(DaoException e){
+			throw new RuntimeException("Unable to add the requested book",e);
+		}
+	}
+
+	@Override
+	public void deleteBook(int id) {
+		idValidation(id);
+		try {
+			bookDao.deleteBook(id);
+		}catch(DaoException e) {
+			throw new RuntimeException("Unable to delete the requested book, book with the given id doen't exist.",e);
+		}
+		
+	}
+
+	@Override
+	public void updateBook(int id, Book book) {
+		idValidation(id);
+		bookValidation(book);
+		try {
+			bookDao.updateBook(id, book);
+		}catch(DaoException e) {
+			throw new RuntimeException("Book with the given ID doesn't exist",e);
+		}
+	}
+
+	@Override
+	public Book getBookById(int id) {
+		idValidation(id);
+		try {
+			return bookDao.getBookById(id);
+		}catch(DaoException e) {
+			throw new RuntimeException("Unable to get the requested book",e);
+		}
+			
+		}
+	
+	public void bookValidation(Book book) {
+		if(book == null) {
+			throw new IllegalArgumentException("Book can't be null");
+		}
+		if(book.getPrice()<=0) {
+			throw new IllegalArgumentException("Price must be positive");
+		}
+		if(book.getBookTitle() == null || book.getBookTitle().trim().isEmpty()) {
+			throw new IllegalArgumentException("Book title must be entered");
+		}
+		if(book.getAuthor() == null || book.getAuthor().trim().isEmpty()) {
+			throw new IllegalArgumentException("Author's name must be entered ");
+		}
+		
+	}
+	public void idValidation(int id) {
+		if(id <=0 ) {
+			throw new IllegalArgumentException("Id entered is invaid");
+		}
+	}
+	
+}
---
+++ labAssignment1_Module2/bookstore_1/src/main/java/com/bookstore/util/ConnectionFactory.java
@@ -0,0 +1,26 @@
+
+
+package com.bookstore.util;
+
+import java.sql.*;
+
+public class ConnectionFactory {
+
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+        	connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1","root","root");        	
+        }catch(SQLException e){
+        	e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
+
